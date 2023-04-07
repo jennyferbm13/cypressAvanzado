a382ea5fbe9627bbb7648e953ca352d04b512b45
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-04-07 07:40:09 -0500

    Configuracion pruebas BDD

--- cypress.config.js
+++ cypress.config.js
@@ -4,34 +4,65 @@ const {
   addMatchImageSnapshotPlugin,
 } = require("cypress-image-snapshot/plugin");
 
+const webpack = require("@cypress/webpack-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+
 const values = [];
+
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here
+  addMatchImageSnapshotPlugin(on, config);
+  config.env.variable = process.env.NODE_ENV ?? "No hay variable";
+  on("task", {
+    guardar(valor) {
+      const key = Object.keys(valor)[0];
+      values[key] = valor[key];
+      return null;
+    },
+    obtener(key) {
+      console.log("values", values);
+      return values[key] ?? "no hay valor";
+    },
+  });
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
     experimentalSessionAndOrigin: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      addMatchImageSnapshotPlugin(on, config);
-      config.env.variable = process.env.NODE_ENV ?? "No hay variable";
-      on("task", {
-        guardar(valor) {
-          const key = Object.keys(valor)[0];
-          values[key] = valor[key];
-          return null;
-        },
-        obtener(key) {
-          console.log("values", values);
-          return values[key] ?? "no hay valor";
-        },
-      });
-      return config;
-    },
     retries: 2,
-  },
-  env: {
-    credentials: {
-      user: "username",
-      password: "password",
+    specPattern: "**/*.feature", //ahora las pruebas que va a detecrtar tienen la extencion .feature
+    setupNodeEvents,
+    env: {
+      credentials: {
+        user: "username",
+        password: "password",
+      },
     },
   },
 });
---
+++ cypress/e2e/features/Login/login.js
@@ -0,0 +1,22 @@
+const {
+  Given,
+  When,
+  Then,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const { loginPage } = require("../../../pageObjects/LoginPage.js");
+
+//crear step, debe conincidir el texto
+
+Given("I am on the login page", () => {
+  loginPage.visit();
+  loginPage.validateLogin();
+});
+When(
+  /^I fill in my email and password with "([^"]*)" and "([^"]*)"$/, //esta diciendo toma lo que esta entre las comillas
+  function (username, password) {
+    loginPage.login(username, password);
+  }
+);
+Then(/^I should validate that I am logged in$/, function () {
+  loginPage.validateSuccessLogin();
+});
